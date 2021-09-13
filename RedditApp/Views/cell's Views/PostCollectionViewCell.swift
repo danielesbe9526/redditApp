8c578f613d1b64ce827f7d22d40804dff48f01c1
dabeltran95 <daniel.beltran@mercadolibre.com.co>
2021-09-13 17:24:34 -0500

    added suport to multiline title and size for collectionView

--- RedditApp/Views/cell's Views/PostCollectionViewCell.swift
+++ RedditApp/Views/cell's Views/PostCollectionViewCell.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class PostCollectionViewCell: UICollectionViewCell {
+
     @IBOutlet weak private var title: UILabel!
     @IBOutlet weak private var author: UILabel!
-    @IBOutlet weak private var numberOfComments: UILabel!
     @IBOutlet weak private var entryDate: UILabel!
-    @IBOutlet weak private var thumbnail: UIImage!
-    @IBOutlet weak private var unreadStatus: UIView!
-    @IBOutlet weak private var comentsTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak private var entryDateTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak private var numberOfComments: UILabel!
+    @IBOutlet weak private var thumbnail: UIImageView!
+    @IBOutlet weak private var readStatus: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureWith(post: ChildData) {
+        title.numberOfLines = 0
+        title.text = post.title
+        self.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
 }
